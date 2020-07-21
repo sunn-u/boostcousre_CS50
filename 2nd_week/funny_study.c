3b9299e9322d24d51668abfb20e80de7498a288e
sunn <68581870+sunn-u@users.noreply.github.com>
2020-07-21 15:51:23 +0900

    Create funny_study.c
    using for & while

---
+++ 2nd_week/funny_study.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    for (int i = 0; i < 10; i++)
+    {
+        printf("개발 공부는 정말 %d만큼 재밌어!\n", i);
+    }
+    int j = 0;
+    while (j < 10)
+    {
+        printf("개발 공부는 또 정말 %d만큼 재밌어!\n", j);
+        j ++;
+    }
+}
