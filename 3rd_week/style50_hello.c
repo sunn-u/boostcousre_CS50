07c1d36dad577f1c63522bc2fc161c8ecf690980
sunn <68581870+sunn-u@users.noreply.github.com>
2020-07-24 21:15:54 +0900

    Create style50_hello.c
    shell : style50_hello.c
    가독성이 좋은, 보기에 편하고 예쁜 코드를 짜는 것도 개발자의 역량

---
+++ 3rd_week/style50_hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{printf("hello, world!\n");}
+
+// style50 style50_hello.c
+// #include <stdio.h>
+// int main(void)
+// {\n
+//     printf("hello, world!\n");\n
+// }
+
+// \n means that you should insert a newline.
+// And consider adding more comments!
