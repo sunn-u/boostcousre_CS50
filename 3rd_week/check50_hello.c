886785a2830e1e97af7f3d4d2756f58a18d95891
sunn <68581870+sunn-u@users.noreply.github.com>
2020-07-24 21:18:23 +0900

    Create check50_hello.c
    shell : check50 home/ubuntu/check50_hello
    주어진 문제를 제대로 해결하기 위해서는 테스트(=점검)가 반드시 필요하다.

---
+++ 3rd_week/check50_hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world!\n");
+}
+
+// check50 : cs50 에서 코드를 채점하기 위한 라이브러리(only in cs50)
+// ex. check50 home/ubuntu/check50_hello
+// cs50 에서만 제공하는 툴이지만 이와 같은 테스트(=점검)가 현실에서도 필요
