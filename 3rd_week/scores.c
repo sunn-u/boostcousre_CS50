0efb651caf47b7c3a2d2d51d7dbc720da8123af4
sunn <68581870+sunn-u@users.noreply.github.com>
2020-07-25 12:40:06 +0900

    Update scores.c
    using global variable, constant variable

--- 3rd_week/scores.c
+++ 3rd_week/scores.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// 전역 변수(global vairable) : 함수 바깥에서 지정하는 변수
+// 상수 : 변하지 않는 값
+const int N = 3;
+
 int main(void)
 {
-    // 배열(array) : ↓ 정수 3개 만큼의 메모리를 내놔!
-    int scores[3];
+    int scores[N];
 
     scores[0] = 72;
     scores[1] = 73;
     scores[2] = 33;
 
-    printf("Average: %i\n", (scores[0] + scores[1] + scores[2])/3);
+    printf("Average: %i\n", (scores[0] + scores[1] + scores[2])/N);
 }
