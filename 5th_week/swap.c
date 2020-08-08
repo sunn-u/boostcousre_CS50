cb59a13d7284ede1a26cc433db1708810f3a25d0
sunn <68581870+sunn-u@users.noreply.github.com>
2020-08-08 17:45:38 +0900

    Create swap.c
    learning about stack, heap
    - heap : 메모리가 쌓이는 공간
    - stack : 호출된 함수(와 변수)가 쌓이는 공간

---
+++ 5th_week/swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 5;
+    int y = 7;
+
+    printf("x: %i, y: %i\n", x,y);
+    swap(&x,&y);
+    printf("x: %i, y: %i\n", x,y);
+}
+
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
